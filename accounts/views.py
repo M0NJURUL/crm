dcb73b2cc391a635b40c06cdeb4ed3c9f317fb6e
m0njurul <monjurul15-10804@diu.edu.bd>
2023-02-27 02:53:13 +0600

    fourth commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 # Create your views here.
 
 def home(request):
     return render(request, 'accounts/dashboard.html')
 
 def product(request):
-    return render(request, 'accounts/product.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/product.html', {'products': products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
