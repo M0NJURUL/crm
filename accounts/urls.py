73f3627841dc0c4a8424819534cc650d6ae617ab
m0njurul <monjurul15-10804@diu.edu.bd>
2023-03-03 20:42:21 +0600

    seventh commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('product/', views.product),
-    path('customer/<str:pk>/', views.customer),
+    path('', views.home, name = 'home'),
+    path('product/', views.product, name = 'product'),
+    path('customer/<str:pk>/', views.customer, name = 'customer'),
+
+    path('create_order/<str:pk>/', views.createOrder, name='create_order'),
+    path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
+    path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
 ]
+
+#<a class="btn btn-primary  btn-sm btn-block" href="{% url 'create_order' %}">Create Order</a>
